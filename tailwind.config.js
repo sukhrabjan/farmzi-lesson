d460f8522b9b2f97857e2f7d381f5c3c926b8df8
sukhrabjan <suhrobamonov550@gmail.com>
2023-12-23 15:29:30 +0500

    I'm halfway through the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,38 @@ module.exports = {
 
       colors: {
         'accent-color': '#177165',
+        'accent2': '#A4D6CF',
+        'accent3':'#177165',
+      },
+      width: {
+        '455': '455px',
+        '170': '170px',
+        '212': '212px',
+      },
+      height: {
+        '390': '390px',
+        '30': '30px',
       },
 
-      width: {
-        '11xl': '1340px',
+      maxWidth: {
+        '8xl': '1340px',
+      },
+      margin: {
+        '15': '60px',
+        '18':'18px' ,
+        '123':'123px' ,
+        
+      },
+      lineHeight: {
+        'extra-loose': '2.5',
+        '12': '26px',
+        'extra-loose': '2.5',
+        '13': '29px',
+        'extra-loose': '2.5',
+        '79': '78px',
       }
-    
+
+
     },
   },
   plugins: [],
